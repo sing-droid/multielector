2321e8ba082ba34e009e786a93709a473d6bd4fe
SONU SINGH <sonusingh82342@gmail.com>
2023-06-18 02:04:48 +0530

    checkbox implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App= ()=> {
+
+  const CountryName = [  //Array for the Country List.
+    "Australia",
+    "Bermuda",
+    "Canada",
+    "Cameroon",
+    "Denmark",
+    "France",
+    "Finland",
+    "Germany",
+    
+  ];
+
+  const selectedCheckboxes = new Set(); //This variable will store lable for the selected checkboxes.
+
+
+  const handleChange = (lable) => {        // function which handle all the selected checkboxes.
+    if(selectedCheckboxes.has(lable)){
+      selectedCheckboxes.delete(lable);
+    }
+    else{
+      selectedCheckboxes.add(lable);
+    }
+
+  }
+
+  const createCheckBox = (lable) => (     // function for create a checkbox.
+    <Checkbox label={lable} onChange={handleChange}/>
+
+  )
+ 
+  return (                   //creqating a list for the checkbox.
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="checkbox-container">
+      {CountryName.map(it =>
+        createCheckBox(it))}
+      </div>
     </div>
   );
 }
+
+const Checkbox = ({label,onChange}) => {   // Resuse tag for the checkbox creation.
+  return(
+    <div>
+  <label>
+    <input
+      type="checkbox"
+      value={label}
+      onChange={onChange}
+      className="common"
+    />
+
+    {label}
+  </label>
+</div>
+  )
+}
+
+
+
 
 export default App;
